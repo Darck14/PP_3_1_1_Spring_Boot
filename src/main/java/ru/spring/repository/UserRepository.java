548f05521ab481c5d5a2483ac66e2bec2a3e006f
Bogatov Dmitiry <darck538567@gmail.com>
2024-08-15 17:36:28 +0400

    Final

--- src/main/java/ru/spring/repository/UserRepository.java
+++ src/main/java/ru/spring/repository/UserRepository.java
@@ -1,15 +1,10 @@
 package ru.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import ru.spring.model.User;
 
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    @Modifying
-//    @Query("UPDATE users u SET u.name = :#{#user.name}, u.sername = :#{#user.sername}, u.sex = :#{#user.sex} WHERE u.id = :#{#user.id}")
-//    void updateUser(User user);
 }
