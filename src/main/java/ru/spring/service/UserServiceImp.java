b4267dfc0bf60b532b53d57b829ec794c0a2735d
Bogatov Dmitiry <darck538567@gmail.com>
2024-08-15 17:33:56 +0400

    Final

--- src/main/java/ru/spring/service/UserServiceImp.java
+++ src/main/java/ru/spring/service/UserServiceImp.java
@@ -3,7 +3,7 @@ package ru.spring.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.spring.dao.UserDao;
+import ru.spring.repository.UserRepository;
 import ru.spring.model.User;
 
 import java.util.List;
@@ -11,44 +11,44 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-    private final UserDao userDao;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImp(UserDao userDao) {
-        this.userDao = userDao;
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Transactional
     @Override
     public void add(User user) {
-        userDao.save(user);
+        userRepository.save(user);
     }
 
     @Override
     public List<User> listUsers() {
-        return userDao.findAll();
+        return userRepository.findAll();
     }
 
     @Override
     public User findById(long id) {
-        return userDao.findById(id).get();
+        return userRepository.findById(id).get();
     }
 
     @Transactional
     @Override
     public void update(User user) {
-        userDao.updateUser(user);
+        userRepository.save(user);
     }
 
     @Transactional
     @Override
     public void deleteById(long id) {
-        userDao.deleteById(id);
+        userRepository.deleteById(id);
     }
 
     @Transactional
     @Override
     public void deleteAll() {
-        userDao.deleteAll();
+        userRepository.deleteAll();
     }
 }
